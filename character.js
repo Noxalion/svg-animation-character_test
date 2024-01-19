e021e06de2ae61dbb760e126e66ecfde3d96f69a
bastien gathoye <bastiengathoye@gmail.com>
2024-01-19 15:08:13 +0100

    tested some animations and transitions don't work

--- character.js
+++ character.js
@@ -1,9 +1,16 @@
 "use strict";
-let btn = document.querySelector(".btnStop");
+let btnStop = document.querySelector(".btnStop");
 let svg = document.querySelector(".svg");
+let btnWave= document.querySelector(".btnWave");
 
-btn.addEventListener("click", stop);
+btnStop.addEventListener("click", stop);
+btnWave.addEventListener("click", wave);
+
 
 function stop(){
     svg.classList.toggle("stop");
+}
+
+function wave(){
+    svg.classList.toggle("wave");
 }
